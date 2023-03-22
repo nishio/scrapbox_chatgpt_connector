6a48e997a658ae0430fa425baa3291feb4f484dc
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2023-03-22 12:57:35 +0900

    update for v2

--- ask.py
+++ ask.py
@@ -1,93 +1,81 @@
-# default args for update_from_scrapbox() as sample
 import os
 import dotenv
 import openai
 import tiktoken
-from make_index import VectorStore
-
-
-INDEX_FILE = "qualia-san.pickle"
-
-dotenv.load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
-enc = tiktoken.get_encoding("cl100k_base")
-
-
-def get_size(text):
-    return len(enc.encode(text))
-
-
-def embed(text):
-    return openai.Embedding.create(
-        input=[text], model="text-embedding-ada-002")["data"][0]["embedding"]
+from make_index import VectorStore, get_size
 
 
 PROMPT = """
-Read the following text and answer the question. Your reply should be shorter than 250 characters.
-
-## Text
+You are virtual character. Read sample output of the character in the following sample section. Then reply to the input.
+## Sample
 {text}
-
-## Question
+## Input
 {input}
-""".strip()
-
-
-input_str = """
-もっとも大事な問いとは何だろう？
 """.strip()
 
 
 MAX_PROMPT_SIZE = 4096
 RETURN_SIZE = 250
-PROMPT_SIZE = get_size(PROMPT)
-rest = MAX_PROMPT_SIZE - RETURN_SIZE - PROMPT_SIZE
-input_size = get_size(input_str)
-print("input size:", input_size)
-if rest < input_size:
-    raise RuntimeError("too large input!")
-
-rest -= input_size
 
 
-vs = VectorStore(INDEX_FILE)
-samples = vs.get_sorted(input_str)
+def ask(input_str, index_file):
+    PROMPT_SIZE = get_size(PROMPT)
+    rest = MAX_PROMPT_SIZE - RETURN_SIZE - PROMPT_SIZE
+    input_size = get_size(input_str)
+    # print("input size:", input_size)
+    if rest < input_size:
+        raise RuntimeError("too large input!")
+    rest -= input_size
 
-to_use = []
-used_title = []
-for _sim, body, title in samples:
-    if title in used_title:
-        continue
-    size = get_size(body)
-    if rest < size:
-        break
-    to_use.append(body)
-    used_title.append(title)
-    print("\nUSE:", title, body)
-    rest -= size
+    vs = VectorStore(index_file)
+    samples = vs.get_sorted(input_str)
 
-text = "\n\n".join(to_use)
-prompt = PROMPT.format(input=input_str, text=text)
+    to_use = []
+    used_title = []
+    for _sim, body, title in samples:
+        if title in used_title:
+            continue
+        size = get_size(body)
+        if rest < size:
+            break
+        to_use.append(body)
+        used_title.append(title)
+        # print("\nUSE:", title, body)
+        rest -= size
+
+    text = "\n\n".join(to_use)
+    prompt = PROMPT.format(input=input_str, text=text)
+
+    # print("\nPROMPT:")
+    # print(prompt)
+
+    print("\nTHINKING...")
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": prompt}
+        ],
+        max_tokens=RETURN_SIZE,
+        temperature=0.0,
+    )
+
+    # print("\nRESPONSE:")
+    # print(response)
+
+    # show question and answer
+    content = response['choices'][0]['message']['content']
+    print("\nANSWER:")
+    print(f">>>> {input_str}")
+    print(">", content)
+
+    # show reference
+    # print("\nREFERENCE:", *[f"[{s}]" for s in used_title])
 
 
-print("\nPROMPT:")
-print(prompt)
+def main():
+    ask("Scrapbox ChatGPT Connectorって何？", "tiny_sample.pickle")
+    ask("クオリアさん、日本語で自己紹介して", "tiny_sample.pickle")
 
-print("\nTHINKING...")
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "user", "content": prompt}
-    ],
-    max_tokens=RETURN_SIZE,
-    temperature=0.0,
-)
 
-print("\nRESPONSE:")
-print(response)
-content = response['choices'][0]['message']['content']
-
-print("\nANSWER:")
-print(f">>>> {input_str}")
-print(">", content)
-print("\nref.", *[f"[{s}]" for s in used_title])
+if __name__ == "__main__":
+    main()
--- make_index.py
+++ make_index.py
@@ -9,13 +9,8 @@ import re
 import dotenv
 import os
 
-# default args for update_from_scrapbox() as sample
-# input json data from https://scrapbox.io/qualia-san/
-JSON_FILE = "from_scrapbox/qualia-san.json"
-# output file name
-INDEX_FILE = "qualia-san.pickle"
-
 BLOCK_SIZE = 500
+EMBED_MAX_SIZE = 8150
 
 dotenv.load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -23,11 +18,12 @@ enc = tiktoken.get_encoding("cl100k_base")
 
 
 def get_size(text):
+    "take text, return number of tokens"
     return len(enc.encode(text))
 
 
-def embed(text):
-    EMBED_MAX_SIZE = 8150  # actual limit is 8191
+def embed_text(text, sleep_after_success=1):
+    "take text, return embedding vector"
     text = text.replace("\n", " ")
     tokens = enc.encode(text)
     if len(tokens) > EMBED_MAX_SIZE:
@@ -38,7 +34,7 @@ def embed(text):
             res = openai.Embedding.create(
                 input=[text],
                 model="text-embedding-ada-002")
-            time.sleep(1)
+            time.sleep(sleep_after_success)
         except Exception as e:
             print(e)
             time.sleep(1)
@@ -48,48 +44,87 @@ def embed(text):
     return res["data"][0]["embedding"]
 
 
-def update_from_scrapbox(name=INDEX_FILE, jsonfile=JSON_FILE):
-    vs = VectorStore(name)
-    data = json.load(open(jsonfile, encoding="utf8"))
+def update_from_scrapbox(json_file, out_index, in_index=None):
+    """
+    out_index: Output index file name
+    json_file: Input JSON file name (from scrapbox)
+    in_index: Optional input index file name. It is not modified and is used as cache to reduce API calls.
+    out_index: 出力インデックスファイル名
+    json_file: 入力JSONファイル名 (scrapboxからの)
+    in_index: オプショナルな入力インデックスファイル名。変更されず、APIコールを減らすためのキャッシュとして使用されます。
+
+    # usage
+    ## create new index
+    update_from_scrapbox(
+        "from_scrapbox/nishio.json",
+        "nishio.pickle")
+
+    ## update index
+    update_from_scrapbox(
+        "from_scrapbox/nishio-0314.json", "nishio-0314.pickle", "nishio-0310.pickle")
+    """
+    if in_index is not None:
+        cache = pickle.load(open(in_index, "rb"))
+    else:
+        cache = None
+
+    vs = VectorStore(out_index)
+    data = json.load(open(json_file, encoding="utf8"))
+
     for p in tqdm(data["pages"]):
         buf = []
         title = p["title"]
         for line in p["lines"]:
-            line = line.strip()
-            line = re.sub(r"https?://[^\s]+", "URL", line)
-            line = re.sub(r"[\s]+", " ", line)
             buf.append(line)
             body = " ".join(buf)
             if get_size(body) > BLOCK_SIZE:
-                vs.get_or_make(body, title)
+                vs.add_record(body, title, cache)
                 buf = buf[len(buf) // 2:]
         body = " ".join(buf).strip()
         if body:
-            vs.get_or_make(body, title)
+            vs.add_record(body, title, cache)
+
+    vs.save()
 
 
 class VectorStore:
-    def __init__(self, name=INDEX_FILE):
+    def __init__(self, name, create_if_not_exist=True):
         self.name = name
         try:
             self.cache = pickle.load(open(self.name, "rb"))
         except FileNotFoundError as e:
-            self.cache = {}
+            if create_if_not_exist:
+                self.cache = {}
+            else:
+                raise
 
-    def get_or_make(self, body, title):
-        if body not in self.cache:
-            self.cache[body] = (embed(body), title)
-            pickle.dump(self.cache, open(self.name, "wb"))
+    def add_record(self, body, title, cache=None):
+        if cache is None:
+            cache = self.cache
+        if body not in cache:
+            # call embedding API
+            self.cache[body] = (embed_text(body), title)
+        elif body not in self.cache:
+            # in cache and not in self.cache: use cached item
+            self.cache[body] = cache[body]
+
         return self.cache[body]
 
     def get_sorted(self, query):
-        q = np.array(embed(query))
+        q = np.array(embed_text(query, sleep_after_success=0))
         buf = []
         for body, (v, title) in tqdm(self.cache.items()):
             buf.append((q.dot(v), body, title))
         buf.sort(reverse=True)
         return buf
 
+    def save(self):
+        pickle.dump(self.cache, open(self.name, "wb"))
+
 
 if __name__ == "__main__":
-    update_from_scrapbox()
+    # Sample default arguments for update_from_scrapbox()
+    JSON_FILE = "from_scrapbox/tiny_sample.json"
+    INDEX_FILE = "tiny_sample.pickle"
+
+    update_from_scrapbox(JSON_FILE, INDEX_FILE)
